ef5d1a5d1fcbdbca7c22454444f766f11effd7a8
AstonDV <astondv.kms@gmail.com>
2023-01-24 22:47:02 +1000

    End task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+int a1 = num / 100;
+int a3 = (num % 100) % 10;
+
+int newNum = (a1 * 10) + a3;
+
+Console.WriteLine($"после удаления второй цифры числа {num} получаем число {newNum}");
