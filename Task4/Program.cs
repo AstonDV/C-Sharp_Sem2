82690d65d21f009e12c1f0dcde88e8ae83b27fd3
AstonDV <astondv.kms@gmail.com>
2023-01-25 00:25:46 +1000

    End task 4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int check1 = num % 7;
+int check2 = num % 23;
+if (check1 == 0 && check2 == 0)
+{
+    Console.WriteLine($"Число {num} кратно одновременно числам 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {num} не кратно одновременно числам 7 и 23");
+}
