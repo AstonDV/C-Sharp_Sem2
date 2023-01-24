f8efd825b8079f1b7d4d83baa09717faba841c80
AstonDV <astondv.kms@gmail.com>
2023-01-25 00:33:50 +1000

    End task 5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 * num1 == num2 || num2 * num2 == num1)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
